78657f470d1214e1c0e9c778f4658f77bf67b73e
Connor Leech <connorleech@gmail.com>
2016-06-04 10:14:30 -0700

    render styles and text to the screen

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom'; // dom stuff in its own package
+
+// import css (webpack puts style tag in javascript and embeds it for us)
+import css from './styles/style.styl';
+
+render(<p>hello</p>, document.getElementById('root'));
