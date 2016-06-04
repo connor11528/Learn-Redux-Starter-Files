b14532b2c3316ddc23a0cc0467f5ac78e95c6a5e
Connor Leech <connorleech@gmail.com>
2016-06-04 10:27:21 -0700

    create the first component and render with hot-reload

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -4,4 +4,7 @@ import { render } from 'react-dom'; // dom stuff in its own package
 // import css (webpack puts style tag in javascript and embeds it for us)
 import css from './styles/style.styl';
 
-render(<p>hello</p>, document.getElementById('root'));
+// Import components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
--- learn-redux/readme.md
+++ learn-redux/readme.md
@@ -8,6 +8,8 @@ Then run `npm start` and open <localhost:7770> in your browser.
 
 This starts an express development server (**devServer.js**).
 
+There is hot reloading for components. (Change components, no need to refresh web browser)
+
 ## Production Build
 
 Run `npm build` to create a distro folder and a bundle.js file.
